4ce3b40ac2ab672c27cbd63be9a8fd62fa3d6931
lucomoro <morelli.2000luca@gmail.com>
2023-06-06 16:40:02 +0200

    Execute code refactoring following SonarCloud issues

--- src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/CPNameAndType.java
+++ src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/CPNameAndType.java
@@ -53,11 +53,6 @@ public class CPNameAndType extends ConstantPoolEntry {
         this.descriptor = Objects.requireNonNull(descriptor, "descriptor");
     }
 
-    /*
-     * field_info { u2 access_flags; u2 name_index; u2 descriptor_index; u2 attributes_count; attribute_info
-     * attributes[attributes_count]; }
-     */
-
     @Override
     public boolean equals(final Object obj) {
         if (this == obj) {
